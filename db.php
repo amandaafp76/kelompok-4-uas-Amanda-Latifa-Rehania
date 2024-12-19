16ac436821b68ac9ceb95901812151f8dccffa19
amandaafp76 <amandaputriapap395@gmail.com>
2024-12-19 13:19:01 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.80.1.133:3310;dbname=db_nilaisiswa',
+    'username' => 'Amandaa',
+    'password' => 'amanda',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
